205bdc50ba6bfb974e3b95e89a938dedbcdf8826
adriano_nokoski@hotmail.com <adriano_novakoski@hotmail.com>
2020-11-06 16:34:51 -0300

    Primeiro commit da aplicação agenda, contemplando já as criações das tabelas padrões django e a criação do app core

---
+++ core/admin.py
@@ -0,0 +1,12 @@
+from typing import Tuple
+
+from django.contrib import admin
+from core.models import Evento
+
+
+# Register your models here.
+class EventoAdmin(admin.ModelAdmin):                                    #Class para difiniar os campos que iram aparecer
+     list_display = ('titulo', 'data_evento', 'date_criacao', 'usuario',)           #Obs no datE_criacao
+     list_filter = ('usuario', 'data_evento',)
+
+admin.site.register(Evento, EventoAdmin)
---
+++ core/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(verbose_name='Evento', max_length=100)               #Campo caracter com maximo de 100
+    descricao = models.TextField(verbose_name='Descrição do evento', blank=True, null=True)   #Campo texto que pode ficar embranco e nulo
+    data_evento = models.DateTimeField(verbose_name='Data do evento')                    #Campo data que não pode ficar em branco
+    date_criacao = models.DateTimeField(verbose_name='Data de criação', auto_now=True)      #Data criação que preeche automatico        #Obs no datE_criacao
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE,verbose_name='Usuário')         #Atribui o evento a um usuáro
+
+    class Meta:
+        db_table = 'evento'                                 #definir o nome do modúlo
+
+    def __str__(self):
+        return self.titulo                                  #Informa o nome de cada agendamento pelo tilulo dele
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y %H:%M')
